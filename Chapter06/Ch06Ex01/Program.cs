26a2c5058ff59cb14782789acce304757b6cfcb0
Ivan Boychuk <boychuk.m.ivan@gmail.com>
2019-05-23 14:26:47 -0500

    Chapter 6 Exercises 1-3 Basic Functions, Exhanging Data

---
+++ Chapter06/Ch06Ex01/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Ivan Boychuk
+ * Chapter 6 - Functions
+ * Defining and Using Basic Functions*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch06Ex01
+{   
+    class Program
+    {
+        static void Write()
+        {
+            WriteLine("Text output from function.");
+        }
+        static void Main(string[] args)
+        {
+            Write();
+            ReadLine();
+        }
+    }
+}
---
+++ Chapter06/Ch06Ex03/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Ivan Boychuk
+ * Chapter 6 - Functions
+ * Exchanging Data with Functions*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch06Ex03
+{
+    class Program
+    {
+        static int SumVals(params int[] vals)//parameter array - defined using 'params', only one per function is allowed
+        {
+            int sum = 0;
+            foreach(int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int sum = SumVals(1, 5, 2, 9, 8);
+            WriteLine($"Summed values = {sum}");
+            ReadKey();
+        }
+    }
+}
