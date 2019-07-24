6007124b2f7c0e92ba962e77424bbf751e180f53
Ivan Boychuk <boychuk.m.ivan@gmail.com>
2019-07-24 17:24:54 -0500

    Chapter 9 Defining Classes

---
+++ Chapter09/Ch09Ex01DefiningClasses/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch09Ex01DefiningClasses
+{
+    class Program
+    {
+        public abstract class MyBase { }
+        internal class MyClass : MyBase { }
+        public interface IMyBaseInterface { }
+        internal interface IMyBaseInterface2 { }
+        internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2 { }
+        internal sealed class MyComplexClass : MyClass, IMyInterface { }
+
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj = new MyComplexClass();
+            WriteLine(myObj.ToString());
+            ReadKey();
+
+        }
+    }
+}
+        
+    
