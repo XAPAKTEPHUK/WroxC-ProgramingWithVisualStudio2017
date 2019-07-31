779030c613f9e82548c74c239962efe3572b22ca
Ivan Boychuk <boychuk.m.ivan@gmail.com>
2019-07-31 11:27:54 -0500

    Ch10Ex02UsingNestedClasses

--- Chapter10/Ch10Ex01UsingFieldsMethodProperties/MyClass.cs
+++ Chapter10/Ch10Ex01UsingFieldsMethodProperties/MyClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ch10Ex01UsingFieldsMethodProperties
 {
-    class MyClass
+    public class MyClass
     {
         public readonly string Name;
         private int intVal;
@@ -22,8 +22,11 @@ namespace Ch10Ex01UsingFieldsMethodProperties
                          "Val must be assigned a value between 0 and 10."));                                                                                                                  
             }
         }
-        public override string ToString() => "Name: " + Name + "\nVal: " + Val;
-        private MyClass() : this("Default Name") { }
+        public override string ToString() => "Name: " + Name + "\nVal: " + Val;//method must be declared using override keyword, 
+                                                        //because it overrides the virtual ToString() method of the base System.Object class
+        private MyClass() : this("Default Name") { } //instatiation oa a class by using nondefault constructor
+                                                    //because the default constructor of MyClass is private
+                                                   //using this("Default Name" endures that Name gets a value if the constructor is called
         public MyClass (String newName)
         {
             Name = newName;
