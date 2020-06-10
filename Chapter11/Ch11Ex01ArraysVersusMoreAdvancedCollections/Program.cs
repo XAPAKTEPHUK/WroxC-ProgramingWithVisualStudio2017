49b4018799b28fb09492f136d9e3db8eebdb54d9
Ivan Boychuk <boychuk.m.ivan@gmail.com>
2020-06-10 17:07:23 -0500

    Chapter 11

---
+++ Chapter11/Ch11Ex01ArraysVersusMoreAdvancedCollections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch11Ex01ArraysVersusMoreAdvancedCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Create an Array type collection of Animal objects and use it");
+            Animal[] animalArray = new Animal[2];
+            Cow myCow1 = new Cow("Lea");
+            animalArray[0] = myCow1;
+            animalArray[1] = new Chicken("Noa");
+            foreach (Animal myAnimal in animalArray)
+            {
+                WriteLine($"New {myAnimal.ToString()} object added to array collection, Name = {myAnimal.Name}");
+            }
+
+        }
+    }
+}
