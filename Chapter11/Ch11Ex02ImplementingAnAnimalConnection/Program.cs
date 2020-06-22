20b9dedd00619b02d67ff263f7237d82b3c01955
Ivan Boychuk <boychuk.m.ivan@gmail.com>
2020-06-22 16:49:39 -0500

    Ch11Ex02Implementin an Animals Collections

---
+++ Chapter11/Ch11Ex02ImplementingAnAnimalConnection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch11Ex02ImplementingAnAnimalConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals animalCollection = new Animals();
+            animalCollection.Add(new Cow("Donna"));
+            animalCollection.Add(new Chicken("Marry"));
+            foreach (Animal myAnimal in animalCollection)
+            {
+                myAnimal.Feed();
+            }
+            ReadKey();
+        }
+    }
+}
